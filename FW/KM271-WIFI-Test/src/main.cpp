c7984627d48cb7b3a548e4ea7e74a7edb1eb408f
Daniel Glaser <7723396+the78mole@users.noreply.github.com>
2023-02-14 21:57:19 +0100

    Upgrade to Hardware Revision 0.0.6

--- FW/KM271-WIFI-Test/src/main.cpp
+++ FW/KM271-WIFI-Test/src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
 
+#ifndef HW_REV
+#warning HW_REV not defined! Please defined in platformio.ini with, e.g. for v0.0.6: -DHW_REV=000006. HW_REV set to default 0.0.6 => 000006
+#define HW_REV 000006   // You can define it in platformio.ini, e.g.: build_flags = -DHW_REV=000006
+#endif
+
 gpio_num_t led1_pin = GPIO_NUM_21;
 gpio_num_t led2_pin = GPIO_NUM_22;
+#if HW_REV <= 000005
 gpio_num_t led3_pin = GPIO_NUM_23;
+#else  // HW_REV_PATCH >= 000006
+gpio_num_t led3_pin = GPIO_NUM_17;
+#endif
 gpio_num_t led4_pin = GPIO_NUM_25;
 
 void setup() {
@@ -23,6 +32,8 @@ void setup() {
 
 void loop() {
   delay(200);
+
+  printf("KM271-Version: v%d.%d.%d", HW_REV / 10000, HW_REV / 100 % 100, HW_REV % 100);
   digitalWrite(led4_pin, HIGH);
   digitalWrite(led1_pin, LOW);
 
